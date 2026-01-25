e89203ab72e43768e04c393048ed0887e5bd1b79
Bryce Heerink <bryceheerink1@gmail.com>
2026-01-25 03:02:45 -0500

    Update Matrix_Creations.cpp
    Created Constructor -- Make the Copy constructor for the basic operatrions

--- Matrix_Creations.cpp
+++ Matrix_Creations.cpp
@@ -1,1 +1,21 @@
-#includ
+#include <iostream>
+#include <vector>
+
+template <typename dtype>
+struct Matrix_Base
+{
+    dtype* data;
+    dtype* rowptr;
+    dtype* colptr;
+    size_t size;
+
+    Matrix_Base(unsigned long m, unsigned long n) :  size(n * m), data(new dtype[m * n])
+    {
+        for(int i = 0; i < size; i++)
+        {
+            data[i] = 0
+        } 
+    }
+
+    
+}
