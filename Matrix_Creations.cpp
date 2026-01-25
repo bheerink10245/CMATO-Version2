12f253c9b1aa2be5ddb5e180718ce91ee5ffe092
Bryce Heerink <bryceheerink1@gmail.com>
2026-01-25 18:00:57 -0500

    Update Matrix_Creations.cpp

--- Matrix_Creations.cpp
+++ Matrix_Creations.cpp
@@ -13,9 +13,32 @@ struct Matrix_Base
     {
         for(int i = 0; i < size; i++)
         {
-            data[i] = 0
+            data[i] = 0;
         } 
     }
 
+    Matrix_Base& opreator=(const Matrix_Base& other)
+    {
+        if(this != &other)
+        {
+            delete [] data;
+            size = other.size;
+            data = new dtype[size];
+            for (size_t i = 0; i < size; i++)
+            {
+                data[i] = other.size[i];
+            }
+
+            return *this;
+        }
+        ~Matrix_Base()
+        {
+            std::cout << "Deleted Matrix @:" << data << "of size" << size(n) << std::endl;
+            delete data[];
+        }
+
+
+    }
+
     
-}
+};
