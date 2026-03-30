30d7804f423add89960e23e064fba8b0f3b21bab
Bryce Heerink <bryceheerink1@gmail.com>
2026-03-30 01:18:45 -0400

    Refactor Array1D template structure and methods

--- Array1D.cpp
+++ Array1D.cpp
@@ -4,14 +4,14 @@
 
 
 
-template <typename dtype>
+template <typename _T>
 struct Array1D
 {
 
-	dtype* data;
+	_T* data;
 	size_t size;
 
-	Array1D(size_t n) : size(n), data(new dtype[n])
+	Array1D(size_t n) : size(n), data(new _T[n])
 	{
 		for (size_t i{ 0 }; i < size; i++)
 		{
@@ -19,7 +19,7 @@ struct Array1D
 		}
 	}
 
-	Array1D(const Array1D& other) : size(other.size), data(new dtype[size]) //copy constructor??
+	Array1D(const Array1D& other) : size(other.size), data(new _T[size]) //copy constructor??
 	{
 		for (size_t i{0}; i < size; i++)
 		{
@@ -32,7 +32,7 @@ struct Array1D
 		{
 			delete[] data;
 			this.size = other.size;
-			this.data = new dtype[size];
+			this.data = new _T[size];
 			for (int i{ 0 }; i < size; i++)
 				data[i] = other.size[i];
 
@@ -47,14 +47,14 @@ struct Array1D
 	}
 };
 
-template <typename dtype>
-Array1D<dtype> Expand(const Array1D<dtype> Original, size_t newSize, dtype fillValue)
+template <typename _T>
+Array1D<_T> Expand(const Array1D<_T> Original, size_t newSize, _T fillValue)
 {
 	if (newSize < Original.size)
 	{
 		throw std::invalid_argument("New size must be larger then original."); 
 	}
-	Array1D<dtype> ExpandedArray(newSize);
+	Array1D<_T> ExpandedArray(newSize);
 	for (size_t i{ 0 }; i < Original.size; i++)
 	{
 		ExpandedArray.data[i] = Original.data[i];
@@ -66,61 +66,61 @@ Array1D<dtype> Expand(const Array1D<dtype> Original, size_t newSize, dtype fillV
 	return ExpandedArray;
 }
 
-template <typename dtype,typename Utype>
-Array1D<std::common_type_t<dtype, Utype>> OneDimAddition(const Array1D<dtype>& Array1, const Array1D<Utype>& Array2) 
+template <typename _T,typename _Y>
+Array1D<std::common_type_t<_T, _Y>> OneDimAddition(const Array1D<_T>& Array1, const Array1D<_Y>& Array2) 
 {
 	if (Array1.size != Array2.size)
 	{
 		throw std::invalid_argument("Arrays must be same size.");
 	}
-	using Result_Type = std::common_type_t<dtype, Utype>;
-	Array1D<Result_Type> ResultantArray(Array1.size);
+	using _RType = std::common_type_t<_T, _Y>;
+	Array1D<_RType> _ResultantA(Array1.size);
 	for (size_t i{ 0 }; i < Array1.size; i++)
 	{
-		ResultantArray.data[i] = Array1.data[i] + Array2.data[i];
+		_ResultantA.data[i] = Array1.data[i] + Array2.data[i];
 	}
-	return ResultantArray;
+	return _ResultantA;
 }
 
-template <typename dtype,typename Utype>
-Array1D<std::common_type_t<dtype,Utype>> OneDimSubtraction(const Array1D<dtype>& Array1, const Array1D<Utype>& Array2) 
+template <typename _T,typename _Y>
+Array1D<std::common_type_t<_T,_Y>> OneDimSubtraction(const Array1D<_T>& Array1, const Array1D<_Y>& Array2) 
 {
 	if (Array1.size != Array2.size)
 	{
 		throw std::invalid_argument("Arrays must be same size.");
     }
-	using Result_Type = std::common_type_t<dtype,Utype> ;
-	Array1D<Result_Type> ResultantArray(Array1.size);
+	using _RType = std::common_type_t<_T,_Y> ;
+	Array1D<_RType> _ResultantA(Array1.size);
 	for (size_t i{ 0 }; i < Array1.size; i++)
 	{
-		ResultantArray.data[i] = Array1.data[i] - Array1.data[i];
+		_ResultantA.data[i] = Array1.data[i] - Array1.data[i];
 
 	}
-	return ResultantArray;
+	return _ResultantA;
 }
 
 
-template <typename dtype,typename Utype>
-Array1D<std::common_type_t<dtype, Utype>> OneDimScalarMultiplication(const Array1D<dtype>& Array1, const long double& Lambda) 
+template <typename _T,typename _Y>
+Array1D<std::common_type_t<_T, _Y>> OneDimScalarMultiplication(const Array1D<_T>& Array1, const long double& Lambda) 
 {
 	if (Lambda == 0)
 	{
 		throw std::error_condition("Scalar can not be 0");
 	}
 
-	using Result_Type = std::common_type_t<dtype,Utype>>;
-	Array1D<Result_Type> ResultantArray(Array1.size);
+	using _RType = std::common_type_t<_T,_Y>>;
+	Array1D<_RType> _ResultantA(Array1.size);
 	for (size_t i{ 0 }; i < Array1.size, i++)
 	{
-		ResultantArray.data[i] = Array1.data[i] * Lambda;
+		_ResultantA.data[i] = Array1.data[i] * Lambda;
 
 	}
-	return ResultantArray;
+	return _ResultantA;
 }
 
 
-template <typename dtype,typename Utype>
-Array1D<std::common_type_t<dtype, Utype>> OneDimCrossProduct(const Array1D<dtype>& Array1, const Array1D<Utype> Array2)
+template <typename _T,typename _Y>
+Array1D<std::common_type_t<_T, _Y>> OneDimCrossProduct(const Array1D<_T>& Array1, const Array1D<_Y> Array2)
 {
 	if (Array1.size > 3)
 	{
@@ -133,30 +133,32 @@ Array1D<std::common_type_t<dtype, Utype>> OneDimCrossProduct(const Array1D<dtype
 	auto ArrayA = (Array1.size < 3) ? Expand(Array1, 3, 0) : Array1;
 	auto ArrayB = (Array2.size < 3) ? Expand(Array2, 3, 0) : Array2;
 
-	using Result_Type = std::common_type_t<dtype, Utype >> ;
-	Array1D<Result_Type> ResultantArray(Array1.size);
+	using _RType = std::common_type_t<_T, _Y >> ;
+	Array1D<_RType> _ResultantA(Array1.size);
 	//hardcode
-	ResultantArray.data[0] = ArrayA.data[1] * ArrayB.data[2] - ArrayA.data[2] * ArrayB.data[1];
-	ResultantArray.data[1] = ArrayA.data[0] * ArrayB.data[2] - ArrayA.data[2] * ArrayB.data[0];
-	ResultantArray.data[2] = ArrayA.data[0] * ArrayB.data[1] - ArrayA.data[1] * ArrayB.data[0];
+	_ResultantA.data[0] = ArrayA.data[1] * ArrayB.data[2] - ArrayA.data[2] * ArrayB.data[1];
+	_ResultantA.data[1] = ArrayA.data[0] * ArrayB.data[2] - ArrayA.data[2] * ArrayB.data[0];
+	_ResultantA.data[2] = ArrayA.data[0] * ArrayB.data[1] - ArrayA.data[1] * ArrayB.data[0];
 
-	return ResultantArray;
+	return _ResultantA;
 }
 
-template <typename dtype, typename Utype>
-std::common_type_t<dtype, Utype> OneDimDotProduct(const Array1D<dtype>& Array1, const Array1D<Utype>& Array2) 
+template <typename _T, typename _Y>
+std::common_type_t<_T, _Y> OneDimDotProduct(const Array1D<_T>& Array1, const Array1D<_Y>& Array2) 
 {
 
 	if (Array1.size != Array2.size)
 	{
 		throw std::invalid_argument("Arrays must be same size");
 	}
-	using Result_Type = std::common_type_t<dtype, Utype>;
-	Result_Type DotProductSum = 0;
+	using _RType = std::common_type_t<_T, _Y>;
+	_RType DotProductSum = 0;
 	for (size_t i{ 0 }; i < Array1.size; i++)
 	{
 		DotProductSum += Array1.data[i] * Array2.data[i];
 	}
 	return DotProductSum;
 }
+
+
 
